46a7b014e523d4fd481b87526c475108734cee3f
rekcah865 <rekcah865@gmail.com>
2015-12-24 13:58:39 +0800

    Create base_setup.sh

---
+++ BackupServer/base_setup.sh
@@ -0,0 +1,5 @@
+## Base image setting
+RUN echo "proxy=http://10.40.3.249:3128" >> /etc/yum.conf
+RUN ln -f /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+RUN echo ZONE="Asia/Shanghai" > /etc/sysconfig/clock
+RUN yum install -y ping which
