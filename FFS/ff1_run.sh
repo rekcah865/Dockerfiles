c7c9f166ea5619c34329ccc51a5f485692e777aa
rekcah865 <rekcah865@gmail.com>
2016-01-11 15:08:29 +0800

    Create ff1_run.sh

---
+++ FFS/ff1_run.sh
@@ -0,0 +1,18 @@
+## prepare FFS software 
+## ./FISFactoryServices
+
+## create volume directory for log and app
+## mkdir -p /vol/app /vol/log
+## chown docker:docker /vol/app vol/log
+
+## build image
+docker build --rm -t ffs1:centos6 ./ffs1.dockerfile
+
+## Sample for container Docker01FFS05
+mkdir -p /vol/app/Docker01FFS05
+#### edit the configuration file under app folder
+## sample: ***.ini, ***.plugin
+## Run container
+docker run -d -l Docker01FFS05 --name=Docker01FFS05 --net=host -v /vol/log:/s01/log -v /vol/app/Docker01FFS05/:/s01/FISFactoryServices/ ffs1:centos6
+
+## End
