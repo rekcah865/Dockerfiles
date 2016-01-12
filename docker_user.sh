19d734644e673a5bbff4e8034baafb94b694efe4
rekcah865 <rekcah865@gmail.com>
2016-01-12 15:28:50 +0800

    Update docker_user.sh

--- docker_user.sh
+++ docker_user.sh
@@ -1,11 +1,11 @@
 # It's to create Docker user on server
 
 groupadd -g 600 docker
-groupmod -g 600 docker
+#groupmod -g 600 docker
 useradd -u 6000 -g docker -G root docker
-## useradd -u 6000 -g docker -G root -d /u01/usr/docker docker
-## usermod -G root docker
+# useradd -u 6000 -g docker -G root -d /u01/usr/docker docker
+# usermod -G root docker
 
-## echo "docker:xxxxxx" |chpasswd
+# echo "docker:xxxxxx" |chpasswd
 
 echo "docker  ALL=(ALL)       NOPASSWD:ALL" >> /etc/sudoers
