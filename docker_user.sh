b5cc3e95ce736ad3b9cc6b5daa439811d2e3d381
rekcah865 <rekcah865@gmail.com>
2016-01-12 15:28:25 +0800

    Update docker_user.sh

--- docker_user.sh
+++ docker_user.sh
@@ -3,6 +3,9 @@
 groupadd -g 600 docker
 groupmod -g 600 docker
 useradd -u 6000 -g docker -G root docker
+## useradd -u 6000 -g docker -G root -d /u01/usr/docker docker
 ## usermod -G root docker
+
+## echo "docker:xxxxxx" |chpasswd
 
 echo "docker  ALL=(ALL)       NOPASSWD:ALL" >> /etc/sudoers
