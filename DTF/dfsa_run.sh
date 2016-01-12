cad4561ccbda74967899edb33d25c886bb0e9e3d
rekcah865 <rekcah865@gmail.com>
2016-01-12 15:51:39 +0800

    Create dfsa_run.sh

---
+++ DTF/dfsa_run.sh
@@ -0,0 +1,14 @@
+## It's to run DFSa container
+
+## Build image
+
+docker build --rm -t dfs_v1:centos6 ./dfs.dockerfile
+
+## Prepare DFS application
+mkdir -p /vol/app/dfsa
+# tar -xf /tmp/dfs.tar /vol/app/dfsa
+
+## Run container
+docker run -d -l dfsa --name=dfsa --net=host -v /vol/app/dfsa:/home/dfs dfs_v1:centos6
+
+## End
